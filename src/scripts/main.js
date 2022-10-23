b15ed647c66262eb08c5c50cfbf6b75a9c80af07
rezafakher <alihash47@yahoo.com>
2022-10-23 15:49:17 +0330

    Fix: Show and hide menu in mobile view

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,15 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function() {
     // Your custom JavaScript goes here
+
+    // Show and hide menu in mobile view
+    document.querySelector('.icon-humburger').addEventListener('click',function(){
+      document.querySelector('.menu__nav').classList.add('u-block');
+    });
+
+    document.querySelector('.icon-close').addEventListener('click',function(){
+      document.querySelector('.menu__nav').classList.remove('u-block');
+    });
+
   });
 })();
