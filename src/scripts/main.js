94dcb82a26a965029427820b84c23ec9c2a6e558
rezafakher <alihash47@yahoo.com>
2022-10-25 16:59:03 +0330

    Fix: Flickity slider for dishes section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,35 @@
 /* eslint-env browser */
-(function() {
+(function () {
   'use strict';
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     // Your custom JavaScript goes here
 
     // Show and hide menu in mobile view
-    document.querySelector('.icon-humburger').addEventListener('click',function(){
+    document.querySelector('.icon-humburger').addEventListener('click', function () {
       document.querySelector('.menu__nav').classList.add('u-block');
     });
 
-    document.querySelector('.icon-close').addEventListener('click',function(){
+    document.querySelector('.icon-close').addEventListener('click', function () {
       document.querySelector('.menu__nav').classList.remove('u-block');
     });
+
+    // Flickity Slider For Dishes
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+      prevNextButtons: false,
+      pageDots: false
+    });
+
+    document.querySelector('.button--next').addEventListener('click', function () {
+      flkty.next();
+    });
+
+    document.querySelector('.button--previous').addEventListener('click', function () {
+      flkty.previous();
+    });
+
+    var flickity_slider = document.querySelector('.flickity-slider').childNodes.length;
+    document.querySelector('.number--Allslide').append(flickity_slider);
 
   });
 })();
